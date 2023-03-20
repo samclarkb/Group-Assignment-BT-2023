cefc7b11fb16e0a9f96e7246fb2b7e8c1689feb3
1190190 <96318680+1190190@users.noreply.github.com>
2023-03-20 16:48:03 +0100

    registreren nu werkend

--- models/models.js
+++ models/models.js
@@ -17,15 +17,6 @@ const Albums = mongoose.model('Albums', albumSchema, 'Albums')
 
 module.exports = Albums
 
-const userSchema = new mongoose.Schema( {
-	Email: String,
-	Like: Array,
-	Password: String,
-	Profilepic: {
-		data: String,
-		contentType: String,
-	},
-	Username: String,
-})
+
 const Users = mongoose.model('Users', userSchema, 'Users')
 module.exports = Users
