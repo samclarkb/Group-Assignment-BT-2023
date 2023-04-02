cddfd6561a170157954502426db1f1481b18f2f1
TejoBurgboy <tejovdburg@gmail.com>
2023-04-02 15:40:41 +0200

    fetch api

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,6 @@ const buttonOne = document.querySelector('#linkOne')
 const buttonTwo = document.querySelector('#linkTwo')
 const buttonThree = document.querySelector('#linkThree')
 const buttonFour = document.querySelector('#linkFour')
-
 // Fallback image of the albumcards
 const lazyBackgrounds = [].slice.call(document.querySelectorAll('.lazy-background'))
 
@@ -70,6 +69,49 @@ if (window.location.href === 'http://localhost:4444/favorites') {
 	buttonThree.classList.add('inActive')
 	buttonFour.classList.add('inActive')
 }
+
+const likeHandler = async (event) => { // stop refresh 
+	event.preventDefault() 
+	// value van de submit button dat de song id is wss
+	 const { value } = event.currentTarget 
+	 console.log('Banaan')
+	 // stop dit in een object
+	 const likeValue = { newLike: value } 
+	 // vul hier je like route in, dus niet /like-game maar de action route van je form
+	const response = await fetch(`/favorites${value}`,
+	 { method: 'POST', headers: {
+		 'Content-Type': 'application/json' 
+		},
+		body: JSON.stringify(likeValue) }) 
+		return response
+	}
+console.log(form);
+
+
+form.forEach(button => button.addEventListener('click', likeHandler))
+
+//async function postData(url = "", data = {}) {
+	//const response = await fetch(url, {
+	  //method: "POST", 
+	  //mode: "cors", 
+	  //cache: "no-cache", 
+	  //credentials: "same-origin", 
+	  //headers: {
+		//"Content-Type": "application/json",
+		
+	  //},
+	  //redirect: "follow", 
+	  //referrerPolicy: "no-referrer", 
+	//});
+	//return response.json(); 
+  //}
+  
+  //postData("http://localhost:3333/favorites:id", { answer: 42 }).then((data) => {
+	//console.log(data); 
+  //});
+
+  //forum.addEventListener("click",postData)
+  
 
 // fetch favorite
 // form.forEach(item => {
