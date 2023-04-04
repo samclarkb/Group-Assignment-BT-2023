c002007576618ce09a06794818be36b5ba5506c6
samclarkb <74268703+samclarkb@users.noreply.github.com>
2023-04-04 17:31:13 +0200

    put spotify function inside script js

--- public/js/script.js
+++ public/js/script.js
@@ -91,3 +91,17 @@ const likeHandler = async event => {
 
 // eventlistener
 form.forEach(button => button.addEventListener('click', likeHandler))
+
+// Spotify API function
+const frame = document.getElementById('embed-frame')
+const spotifyUrl = document.getElementById('spotifyUrl').value
+
+const changeSong = async url => {
+	fetch(`https://open.spotify.com/oembed?url=${url}`)
+		.then(response => response.json())
+		.then(data => {
+			frame.innerHTML = data.html
+		})
+}
+
+changeSong(spotifyUrl)
