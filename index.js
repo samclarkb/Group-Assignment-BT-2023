422ede1177b3b0b5987636159c38b830f31776ee
N13L5A97 <niels.aling@hotmail.com>
2023-04-05 14:00:50 +0200

    empty new password fix

--- index.js
+++ index.js
@@ -292,10 +292,7 @@ app.post('/home', async (req, res) => {
 				currentUser[0].Password
 			)
 			//hash new password
-			const hashedPwd =
-				req.body.newPassword === ''
-					? currentUser[0].Password
-					: await bcrypt.hash(req.body.newPassword, saltRounds)
+			const hashedPwd = await bcrypt.hash(req.body.newPassword, saltRounds)
 			var newPassword = { $set: { Password: hashedPwd } }
 
 			//if profile picture is empty keep current profile picture
@@ -335,6 +332,9 @@ app.post('/home', async (req, res) => {
 				(req.body.currentPassword !== '' && hashCheck === false) ||
 				(req.body.currentPassword == '' && req.body.newPassword !== '')
 			) {
+				return res.render('update', { data: currentUser, passError: true })
+			} 
+			else if (hashCheck === true && req.body.newPassword == '') {
 				return res.render('update', { data: currentUser, passError: true })
 			}
 
